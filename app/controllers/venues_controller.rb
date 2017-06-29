bb3f33e774340c1a154096c1e7787e9706f3b3fa
Loi Tran <lvt15@my.fsu.edu>
2017-06-29 12:22:27 +0700

    Adding venue creation feature for controller, model, view

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,10 +1,26 @@
 class VenuesController < ApplicationController
   def index
+    @venues = Venue.all
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.create(venue_params)
+    flash[:success] = "Venue Created!"
+    redirect_to root_path
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def venue_params
+    params.require(:venue).permit(:name, :full_address, :region_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-
   devise_for :admins
   get "upcoming" => "events#index"
   resources :tickets
   resources :venues
-  resources :users
 
   devise_for :users
     root 'events#index'
     get 'tickets/index' => 'tickets#index'
     get 'tickets/show' => 'tickets#show'
     get 'users/sign_out' => 'sessions#destroy'
+    get 'users' => 'users#index'
 
   resources :events do
     resources :tickets
