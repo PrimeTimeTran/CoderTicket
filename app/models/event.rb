a15e2b4207b90d6c1aa4ad39736c145198758ac8
Loi Tran <lvt15@my.fsu.edu>
2017-07-01 15:18:24 +0700

    Added validation for event creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,29 @@
 class Event < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+
   belongs_to :venue
   belongs_to :category
   belongs_to :user
   has_many :ticket_types
 
+  validate :ends_at_after_starts_at, :future_date
   validates_presence_of :extended_html_description, :venue, :category, :starts_at
   validates_uniqueness_of :name, uniqueness: {scope: [:venue, :starts_at]}
 
   def self.upcoming
-    Event.where("ends_at > ?", Time.now)
+    Event.where("starts_at > ?", Time.now)
+  end
+
+  def ends_at_after_starts_at
+    unless starts_at < ends_at
+      errors.add(:ending_date, "can't be before starting date.")
+    end
+  end
+  
+  def future_date
+    if starts_at < Time.now
+      errors.add(:starting_date, "must be in the future.")
+    end
   end
 
   def self.search(search)
