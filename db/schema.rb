a1084b4ebad1f1e3104b7bcdc10ac1355bef4e84
Loi Tran <lvt15@my.fsu.edu>
2017-07-02 18:50:06 +0700

    reset before deleting entire form to start from scratch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701101051) do
+ActiveRecord::Schema.define(version: 20170702062142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20170701101051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "quantity"
     t.index ["event_id"], name: "index_orders_on_event_id", using: :btree
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
